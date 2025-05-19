a2d4a47fa8c9ac6bc3f0b1739a806008a7b9d196
2sh <git@2sh.me>
2025-05-19 10:48:35 +0100

    Improve wording

--- dev/App.vue
+++ dev/App.vue
@@ -538,7 +538,7 @@ Also `T` for <i>T</i>en. Other design ideas: <sst pin="10011100"/>
 looks like a C, <sst pin="10111100"/> looks like a `G`,
 and <sst pin="0001110"/> and <sst pin="00111100"/> look like `L` and `l`.
 
-Heavily considered were <sst pin="01011010"/> & <sst pin="00011110"/>, a
+Strongly considered were <sst pin="01011010"/> & <sst pin="00011110"/>, a
 <a href="http://www.dozenalsociety.org.uk/basicstuff/hammond.html">proposal</a>
 by Don Hammond but amongst the other numbers 0-9, they just don't look "full"
 enough, like lower case letters (`z` and `t`).
